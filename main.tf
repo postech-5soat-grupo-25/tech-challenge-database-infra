ea6da8f39ed6a5229fb557df6c0ea59f7e0fea6c
Alan Molina <mmolina.alan@gmail.com>
2024-04-22 23:21:39 -0300

    adicionando arquivos terraform e ci/cd

---
+++ main.tf
@@ -0,0 +1,25 @@
+# Define a região da AWS onde os recursos serão criados
+provider "aws" {
+  region = var.aws_region
+}
+
+# Prefixo local para nomear os recursos e facilitar a identificação
+locals {
+  prefix = "postech-5soat-grupo-25"
+}
+
+# Cria uma instância do banco de dados PostgreSQL na AWS RDS
+resource "aws_db_instance" "postgres" {
+  identifier           = "${local.prefix}-rds"
+  engine               = "postgres"
+  username             = var.AWS_DB_USERNAME
+  password             = var.AWS_DB_PASSWORD
+  db_name              = var.db_name
+  allocated_storage    = var.db_allocated_storage
+  engine_version       = var.db_engine_version
+  instance_class       = var.db_instance_class
+  parameter_group_name = var.db_parameter_group_name
+  publicly_accessible  = var.db_publicly_accessible
+  storage_type         = "gp2"
+  skip_final_snapshot  = true
+}
---
+++ variables.tf
@@ -0,0 +1,53 @@
+variable "aws_region" {
+  description = "Região da AWS onde os recursos serão criados."
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "AWS_DB_USERNAME" {
+  description = "Nome de usuário para o administrador do banco de dados. Este usuário terá permissões totais dentro do banco de dados criado."
+  type        = string
+  sensitive   = true
+}
+
+variable "AWS_DB_PASSWORD" {
+  description = "Senha para o administrador do banco de dados. Deve ser uma senha forte para garantir a segurança do banco de dados."
+  type        = string
+  sensitive   = true
+}
+
+variable "db_name" {
+  description = "Nome do banco de dados a ser criado na instância do RDS. Este nome será usado para conectar ao banco de dados."
+  type        = string
+  default     = "postgres"
+}
+
+variable "db_allocated_storage" {
+  description = "O espaço de armazenamento alocado para o banco de dados, em gigabytes (GB). Isso define o tamanho inicial do banco de dados."
+  type        = number
+  default     = 5
+}
+
+variable "db_instance_class" {
+  description = "O tipo de instância do RDS que define a capacidade computacional (CPU, memória). 'db.t3.micro' é adequado para ambientes de teste ou pequenas aplicações (free tier)."
+  type        = string
+  default     = "db.t3.micro"
+}
+
+variable "db_engine_version" {
+  description = "A versão do motor do PostgreSQL a ser utilizada."
+  type        = string
+  default     = "15.6"
+}
+
+variable "db_parameter_group_name" {
+  description = "Nome do grupo de parâmetros a ser associado com esta instância do RDS."
+  type        = string
+  default     = "default.postgres15"
+}
+
+variable "db_publicly_accessible" {
+  description = "Booleano para controlar se a instância do banco de dados será acessível publicamente. Definido como 'false' por padrão - permite acessos apenas internamente na VPC."
+  type        = bool
+  default     = false
+}
