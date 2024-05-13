4ca1388568067f2841715eaba852030691a0a527
Matheus Bachiste Lopes <matheusblopes10@gmail.com>
2024-05-13 19:33:13 -0300

    feat: update infra

--- providers.tf
+++ providers.tf
@@ -1,4 +1,19 @@
 terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "us-east-1"
+}
+
+terraform {
   backend "s3" {
     bucket         = "postech-5soat-grupo-25-tfstate"
     key            = "database/state"
